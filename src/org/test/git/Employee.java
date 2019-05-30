11679aa814cb0baaad0ad4934a52a0509213edd2
Bharath <kumarbharath410@gmai.com>
2019-05-30 14:30:12 +0530

    First class add

---
+++ src/org/test/git/Employee.java
@@ -0,0 +1,16 @@
+package org.test.git;
+
+public class Employee {
+	private void empId() {
+		System.out.println("235346");
+	}
+	public void empName() {
+		System.out.println("Bharath");
+	}
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		e.empId();
+		e.empName();
+	}
+
+}
